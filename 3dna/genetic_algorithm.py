7f5148b8741c6cc526b56cf09997bca17bdebe14
Javier TORO B <javier-enrique.toro-berrios@student-cs.fr>
2024-01-30 10:45:21 +0100

    Initialization of Population

--- 3dna/genetic_algorithm.py
+++ 3dna/genetic_algorithm.py
@@ -3,9 +3,10 @@ from .RotTable import RotTable
 from .Traj3D import Traj3D
 
 class GeneticAlgorithm:
-    def __init__(self, population_size) -> None:
+    def __init__(self, population_size,og_table) -> None:
         self.__population_size = population_size
         self._population = []
+        self.og_table=og_table
         self._populate()
 
     @property
@@ -17,7 +18,8 @@ class GeneticAlgorithm:
         self._population = new_population
 
     def _populate(self):
-        self.population += [getrandbits(32) for _ in range(self.__population_size)]
+        self.population += [uniform_mutation(self.population) for _ in range(self.__population_size)]
+        self.population.append(self.og_table)
 
 def gaussian_mutation(table):
     new_table = table.copy()
@@ -28,10 +30,11 @@ def gaussian_mutation(table):
     return new_table
 
 def uniform_mutation(table):
+    #uniform: (-2sigma,+2sigma)
     new_table = table.copy()
     for dinucleotide in table:
         for i in range(2):
-            lb=-dinucleotide[i+3]
-            ub=+dinucleotide[i+3]
+            lb=-2*dinucleotide[i+3]
+            ub=2*dinucleotide[i+3]
             new_table[dinucleotide][i] += uniform(lb,ub)
     return new_table
