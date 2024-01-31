5ef002c1ab758302cf3591d107a8ef28cf4c85df
Santos Da Rosa Junior Alexsandro <alexsandro.santos-da-rosa-jr@student-cs.fr>
2024-01-31 10:57:19 +0100

    Implemeting test for _populate method

--- 3dna/test_genetic_algorithm.py
+++ 3dna/test_genetic_algorithm.py
@@ -1,6 +1,22 @@
 import unittest
-from genetic_algorithm import *
+from .genetic_algorithm import *
+from .RotTable import RotTable
 
-class testGeneteicAlgorithm(unittest.TestCase):
+class TestGeneticAlgorithm(unittest.TestCase):
     def test_populate(self):
-        pass
+        rot_table = RotTable()
+        population_size1 = 10
+        gen_alg1 = GeneticAlgorithm(population_size1, rot_table)
+        self.assertEqual(len(gen_alg1.population), population_size1)
+        for table in gen_alg1.population:
+            self.assertIsNotNone(table)
+            for row in table.getTable().values():
+                self.assertIsNotNone(row)
+        
+        population_size2 = 50
+        gen_alg2 = GeneticAlgorithm(population_size2, rot_table)
+        self.assertEqual(len(gen_alg2.population), population_size2)
+        for table in gen_alg2.population:
+            self.assertIsNotNone(table)
+            for row in table.getTable().values():
+                self.assertIsNotNone(row)
